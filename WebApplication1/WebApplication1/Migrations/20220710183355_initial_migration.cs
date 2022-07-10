4b177290bf87e89e3fbd99ce1667959976847042
Palak Garg <gargplk1@gmail.com>
2022-07-11 00:08:43 +0530

    migration added

---
+++ WebApplication1/WebApplication1/Migrations/20220710183355_initial_migration.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApplication1.Migrations
+{
+    public partial class initial_migration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Empclass",
+                columns: table => new
+                {
+                    EId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    EName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    EDesignation = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    EDoj = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Empclass", x => x.EId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Empclass");
+        }
+    }
+}
